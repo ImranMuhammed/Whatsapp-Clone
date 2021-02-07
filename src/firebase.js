658a26d83b24f1c26bb46c0eca4b6d925f8f1f06
Mohammed Imran <imranmhd600@gmail.com>
2021-02-07 12:14:16 +0530

    First commit- Whatsapp react app is created

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBLL5j1VQgi2a-enOaLnlK45LKxhst9lxc",
+    authDomain: "whatsapp-mern-97b9c.firebaseapp.com",
+    projectId: "whatsapp-mern-97b9c",
+    storageBucket: "whatsapp-mern-97b9c.appspot.com",
+    messagingSenderId: "737366483956",
+    appId: "1:737366483956:web:dd0f5ad96f622c48e5cfa1",
+    measurementId: "G-LDZBTMLYK5"
+  };
